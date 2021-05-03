642f039173e1ed5fa01b403cf2a9c0313380cb46
Jhianpol <jhianpol.ramos@vallegrande.edu.pe>
2021-05-03 15:55:10 -0500

    Mantenimiento de la tabla Persona

---
+++ Sisregvg_Maven/src/main/java/dao/LocalImpl.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.util.List;
+
+public class LocalImpl extends Conexion implements ICRUD {
+
+    @Override
+    public void registrar(Object obj) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificar(Object obj) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Object obj) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List listarTodos() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+    
+}
