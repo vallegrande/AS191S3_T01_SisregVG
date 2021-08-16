f5d93c1538b903e9f8caf0d670c5915d00d4cd1a
Jhianpol Ramos <72884970+jhianpolmaximilianoramosgil@users.noreply.github.com>
2021-08-16 07:57:10 -0500

    Add files via upload

---
+++ Sisregvg_Maven/src/main/java/modelo/Personal.java
@@ -0,0 +1,19 @@
+package modelo;
+
+
+import lombok.Data;
+@Data
+
+
+public class Personal {
+    private Integer idper;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String celular;
+    private String email;
+    private String domper;
+    private String sexo;
+    private String cargo;
+    private String estper;
+}
