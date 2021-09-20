ab151e398e9daef75265869ca8486baf9697bc7e
Jhianpol <jhianpol.ramos@vallegrande.edu.pe>
2021-09-20 02:45:17 -0500

    Sisregvg con Oracle

---
+++ Sisregvg_Maven/src/main/java/controlador/DeJuradaC.java
@@ -0,0 +1,129 @@
+package controlador;
+
+import dao.DeJuradaImpl;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import java.sql.SQLException;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import modelo.DeJurada;
+
+//Notación CDI
+@Named(value = "DeJuradaC")
+//@Dependent
+@SessionScoped
+public class DeJuradaC implements Serializable {
+
+    private DeJurada declaracion;
+    private DeJuradaImpl dao;
+    private List<DeJurada> listadoDec;
+
+    public DeJuradaC() {
+        declaracion = new DeJurada();
+        dao = new DeJuradaImpl();
+    }
+
+    public void registrar() throws Exception {
+        try {
+            declaracion.setNomper(dao.obtenerNomper(declaracion.getNomper()));          
+            dao.registrar(declaracion);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "OK", "Registrado con Exito"));
+            limpiar();
+            listar();
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Error al registrar"));
+            System.out.println("Error en RegistrarC " + e.getMessage());
+        }
+    }
+     
+
+    public void modificar() throws Exception {
+        try {
+            dao.modificar(declaracion);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "OK", "Modificado con éxito"));
+            limpiar();
+            listar();
+        } catch (Exception e) {
+            System.out.println("Error en modificarC " + e.getMessage());
+        }
+    }
+
+    public void eliminar() throws Exception {
+        try {
+            dao.eliminar(declaracion);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "OK", "Eliminado con éxito"));
+            limpiar();
+            listar();
+        } catch (Exception e) {
+            System.out.println("Error en eliminarC " + e.getMessage());
+        }
+    }
+    
+     public List<String> completeTextDeJurada(String query) throws SQLException, Exception {
+        DeJuradaImpl daoPersonal = new DeJuradaImpl();
+        return daoPersonal.autocompletePersonal(query);
+    }  
+
+//       public void eliminarEstado(PersonaM clie) throws Exception{
+//        try {            
+//            dao.EliminarEstado(clie);
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "OK", "Eliminado con éxito"));
+//            limpiar();
+//            listar();
+//        } catch (Exception e) {
+//            System.out.println("Error en eliminarEstadoC " + e.getMessage());
+//        }
+//    }
+//    
+//    public void restaurarEstado(PersonaM clie) throws Exception{
+//        try {            
+//            dao.RestaurarEstado(clie);
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "OK", "Restaurado con Exito"));
+//            limpiar();
+//            listar();
+//        } catch (Exception e) {
+//            System.out.println("Error en restaurarEstadoC " + e.getMessage());
+//        }
+//    }
+    public void limpiar() {
+        declaracion = new DeJurada();
+    }
+
+    public void listar() {
+        try {
+            listadoDec = dao.listarTodos();
+        } catch (Exception e) {
+            System.out.println("Error en listarC " + e.getMessage());
+        }
+    }
+//metodos generados
+
+    public DeJurada getDeclaracion() {
+        return declaracion;
+    }
+
+    public void setDeclaracion(DeJurada declaracion) {
+        this.declaracion = declaracion;
+    }
+
+    public DeJuradaImpl getDao() {
+        return dao;
+    }
+
+    public void setDao(DeJuradaImpl dao) {
+        this.dao = dao;
+    }
+
+    public List<DeJurada> getListadoDec() {
+        return listadoDec;
+    }
+
+    public void setListadoDec(List<DeJurada> listadoDec) {
+        this.listadoDec = listadoDec;
+    }
+
+  
+ 
+}
---
+++ Sisregvg_Maven/src/main/java/servicios/EmailS.java
@@ -0,0 +1,73 @@
+package servicios;
+
+import java.util.Date;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.swing.JOptionPane;
+import modelo.Personal;
+
+public class EmailS {
+
+    public static void enviarContraseña(Personal per) {  
+        String remitente = "jhianpol.ramos@gmail.com";
+        String clave = "Ramos2021...";
+        String destinatario = per.getEmail();
+        String asunto = "Envio de la contraseña";
+        String cuerpo = " Mi contraseña de ingreso a la tabla usuario es 13579";
+        Properties props = new Properties();
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.user", remitente);
+        props.put("mail.smtp.clave", clave);
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+        try { 
+            String url = "D:\\ADOBE ILUSTRATOR\\logotipo.png";
+            message.setFrom(new InternetAddress(remitente));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
+            message.setSubject(asunto);
+            BodyPart texto = new MimeBodyPart();
+            texto.setText(cuerpo);
+
+            MimeMultipart multiParte = new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            message.setContent(multiParte);
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", remitente, clave);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            System.out.println("Error verifica: " + ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Personal per = new Personal();
+            per.setEmail("jhianpol.ramos@vallegrande.edu.pe");
+            EmailS.enviarContraseña(per);
+            System.out.println("CORREO ENVIADO");
+            JOptionPane.showMessageDialog(null, "CORREO ENVIADO", "CORRECTO", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception ex) {
+            System.out.println("Error en mandarCorreo/mail " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "NO SE ENVIO EL CORREO", "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+}
